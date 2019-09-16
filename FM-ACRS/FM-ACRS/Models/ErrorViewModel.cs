ae263d404d1c0f0aa9b64022aa248550557a15ea
debrasray <debra.ray@gsa.gov>
2019-09-16 08:40:35 -0600

    Solution Creation.

---
+++ FM-ACRS/FM-ACRS/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FM_ACRS.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
